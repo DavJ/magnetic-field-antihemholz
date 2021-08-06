008244f0ccd60b95a54d7178b4bf035a8fe5ee89
David Jaros <jdavid.cz@gmail.com>
2021-08-06 22:54:31 +0200

    progress

--- field_calculator.py
+++ field_calculator.py
@@ -5,6 +5,23 @@ from pylab import plot, xlabel, ylabel, suptitle, legend, show
 K = lambda k: ellipk(k ** 2.0)  # Elliptic integral, first kind, as a function of k
 E = lambda k: ellipe(k ** 2.0)  # Elliptic integral, second kind, as a function of k
 
+
+class AntiHemholtz:
+
+    def __init__(self, B0, loop_radius, axial_offset):
+        self.B0 = B0
+        self.loop_radius = loop_radius
+        self.axial_offset - axial_offset
+        self.loop_clockwise = LoopField(B0, loop_radius, axial_offset)
+        self.loop_anticlockwise = LoopField(-B0, loop_radius, -axial_offset)
+
+    def Bz(self, r, z):
+        return self.loop_clockwise.Bz(r, z) + self.loop_aniclockwise.Bz(r, z)
+
+    def Br(self, r, z):
+        return self.loop_clockwise.Br(r, z) + self.loop_aniclockwise.Br(r, z)
+
+
 class LoopField:
 
     def __init__(self, B0, loop_radius, axial_offset):
@@ -13,7 +30,7 @@ class LoopField:
         self.axial_offset - axial_offset
 
     def cylindrical(self, r=0, fi=0, z=0):
-        return (self.Bx(), 0, self.Bz(r, z))
+        return (self.Br(r,z), 0, self.Bz(r, z))
 
     def alpha(self, r):
         return r / self.loop_radius
@@ -62,9 +79,3 @@ class LoopField:
                    (E(self.k(r, z)) * ((1.0 + self.alpha(r) ** 2 + self.beta(z) ** 2) / (self.q(r, z) - 4 * self.alpha(r))) - K(
                        self.k(r, z))) \
                    / pi / sqrt(self.q(r, z))
-
-
-
-
-
-
